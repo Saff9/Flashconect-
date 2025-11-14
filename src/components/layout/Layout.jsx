114dcc4cb185d244419b2708c29a9dab37448177
Saff9 <saffanakbar942@gmail.com>
2025-11-14 15:32:50 +0530

    Create Layout.jsx

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
